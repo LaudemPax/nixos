eced28ea9dff287cc75ac4ed061d11591b121989
LaudemPax <aman.tifli@gmail.com>
2024-03-09 23:33:33 +0100

    removed neovim conf

--- configuration.nix
+++ configuration.nix
@@ -6,7 +6,8 @@
 {
   imports =
     [ # Include the results of the hardware scan.
-      /etc/nixos/hardware-configuration.nix
+      # /etc/nixos/hardware-configuration.nix
+      ./hardware-configuration.nix
       inputs.home-manager.nixosModules.default
     ];
 
--- home.nix
+++ home.nix
@@ -80,25 +80,4 @@
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
-
-  # zsh
-#  programs.zsh = {
-#  plugins = [
-#	{
-#		name = "powerlevel10k";
-#		src = pkgs.zsh-powerlevel10k;
-#		file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
-#	}
-#	{
-#		name = "powerlevel10k-config";
-#		src = lib.cleanSource ./p10k-config;
-#		file = "p10k.zsh";
-#	}
-#  ];
-#  enable = true;
-#    oh-my-zsh = {
-#	enable = true;
-#	plugins = [ "git" ];
-#    };
-#  };
 }

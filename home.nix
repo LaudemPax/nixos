16bf60989a5de9fd718dd8641f3de0b22fc1f1a6
LaudemPax <aman.tifli@gmail.com>
2024-03-11 20:15:11 +0100

    syncthing is now a service

--- home.nix
+++ home.nix
@@ -48,12 +48,20 @@
     pkgs.logseq
     pkgs.discord
     pkgs.steam
-    pkgs.syncthing
     pkgs.cargo
     pkgs.nodejs_21
     pkgs.lazygit
     pkgs.latte-dock
   ];
+
+  services = {
+    syncthing = {
+        enable = true;
+        user = "myusername";
+        dataDir = "/home/myusername/Documents";    # Default folder for new synced folders
+        configDir = "/home/myusername/Documents/.config/syncthing";   # Folder for Syncthing's settings and keys
+    };
+  };
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.

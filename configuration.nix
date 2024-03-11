b2d0a2bc477d35d293f5c09abeda87155b5477d7
LaudemPax <aman.tifli@gmail.com>
2024-03-11 12:19:18 +0100

    using KDE

--- configuration.nix
+++ configuration.nix
@@ -65,8 +65,12 @@
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  # services.xserver.displayManager.gdm.enable = true;
+  # services.xserver.desktopManager.gnome.enable = true;
+
+  # Enable KDE Plasma
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
 
   # Configure keymap in X11
   services.xserver = {

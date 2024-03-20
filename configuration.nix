f938b65c5c77783b78d0ff107ac66e9826126dd1
LaudemPax <aman.tifli@gmail.com>
2024-03-20 17:19:09 +0100

    enable docker

--- configuration.nix
+++ configuration.nix
@@ -195,6 +195,9 @@
   # flatpak
   services.flatpak.enable = true;
 
+# enable docker
+virtualisation.docker.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;

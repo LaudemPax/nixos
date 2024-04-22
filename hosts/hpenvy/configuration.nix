397ed608578bd73d8951bae74055a5ac001da28b
LaudemPax <aman.tifli@gmail.com>
2024-04-23 01:00:28 +0200

    removed more nvidia stuff

--- hosts/hpenvy/configuration.nix
+++ hosts/hpenvy/configuration.nix
@@ -25,7 +25,7 @@
 	driSupport = true;
 	driSupport32Bit = true;
   };
-  services.xserver.videoDrivers = ["nvidia"];
+#  services.xserver.videoDrivers = ["nvidia"];
 #  hardware.nvidia = {
 #	modesetting.enable = true;
 #	powerManagement.enable = false;

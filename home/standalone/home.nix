c81259913c8ef35d2184f7c812dd93e4171f910c
Aman Tifli <aman.tifli@gmail.com>
2024-04-27 01:18:51 +0200

    ohmyzsh

--- home/standalone/home.nix
+++ home/standalone/home.nix
@@ -50,8 +50,19 @@
     # '')
   ];
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
+  # some extra zshell config
+  programs.zsh = {
+      enableCompletion = true;
+      autosuggestions.enable = true;
+      syntaxHighlighting.enable = true;
+      ohMyZsh = {
+          enable = true;
+          plugins = [ "git" "sudo" "docker" ];
+      };
+  };
+
+# Home Manager is pretty good at managing dotfiles. The primary way to manage
+# plain files is through 'home.file'.
   home.file = {
     # # Building this configuration will create a copy of 'dotfiles/screenrc' in
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a

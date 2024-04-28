fd3cf5706b300a7ad78e259571ddc3895f714617
LaudemPax <aman.tifli@gmail.com>
2024-04-28 22:44:00 +0200

    added onlyoffice

--- hosts/workstation/home.nix
+++ hosts/workstation/home.nix
@@ -56,6 +56,7 @@
             pkgs.lazygit
             pkgs.latte-dock
             pkgs.teamspeak_client
+            pkgs.onlyoffice-bin_7_5
             ];
 
 # Home Manager is pretty good at managing dotfiles. The primary way to manage

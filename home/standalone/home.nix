e077ac0d4dcce261e41f89b912d879d6bb1c4bdc
Muhammad Aman Tifli <ge84gex@mytum.de>
2024-05-15 16:17:03 +0200

    addlattedock

--- home/standalone/home.nix
+++ home/standalone/home.nix
@@ -38,6 +38,7 @@
     pkgs.google-chrome
 # for  neovim copypasting
     pkgs.xclip
+    pkgs.latte-dock
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the

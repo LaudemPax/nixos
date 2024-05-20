9ca49579127ec07c24e8c6647b45b0a5e8f7a061
Aman Tifli <aman.tifli@gmail.com>
2024-05-20 12:21:22 +0200

    added tmux config

--- home/standalone/home.nix
+++ home/standalone/home.nix
@@ -40,7 +40,7 @@
     pkgs.xclip
     pkgs.latte-dock
     pkgs.ranger
-    pkgs.tmux
+    # pkgs.tmux
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
@@ -61,6 +61,35 @@
       enableCompletion = true;
       autosuggestion.enable = true;
       syntaxHighlighting.enable = true;
+  };
+
+  # tmux config
+  programs.tmux = {
+      enable = true;
+      extraConfig = ''
+          # Use 256 colors for terminal
+          set -g default-terminal "screen-256color"
+
+          # If you still face issues, uncomment the following line to override terminal capabilities
+          set -ga terminal-overrides ',xterm-256color:Tc,konsole-256color:Tc'
+
+          # Change the modifier key to <Ctrl+Space>
+          unbind C-b
+          set -g prefix C-Space
+          bind C-Space send-prefix
+
+          # Change window vertical split to mod+v, horizontal split to mod+h
+          unbind '"'
+          unbind %
+          bind v split-window -h
+          bind h split-window -v
+
+          # Rebind moving to the corresponding pane using prefix + arrow keys
+          bind Left select-pane -L
+          bind Right select-pane -R
+          bind Up select-pane -U
+          bind Down select-pane -D
+          '';
   };
 
 # Home Manager is pretty good at managing dotfiles. The primary way to manage

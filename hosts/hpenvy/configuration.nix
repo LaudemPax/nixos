f75badb0fc2f88c58384c0da144c226c476413ea
aman <aman.tifli@gmail.com>
2024-06-25 18:57:18 +0200

    add qemu support

--- hosts/hpenvy/configuration.nix
+++ hosts/hpenvy/configuration.nix
@@ -174,6 +174,12 @@
   gparted
   pciutils
   gcc
+  qemu
+  (pkgs.writeShellScriptBin "qemu-system-x86_64-uefi" ''
+        qemu-system-x86_64 \
+          -bios ${pkgs.OVMF.fd}/FV/OVMF.fd \
+          "$@"
+      '')
   ];
 
   # enable kde connect
